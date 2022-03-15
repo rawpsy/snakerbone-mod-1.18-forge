b727aea0e3d352451cde45bf8c757ccc7feb29de
rawpsy <raxxtq@gmail.com>
2022-03-15 16:43:31 +1100

    Update ModBlocks.java

--- src/main/java/au/com/sb/block/ModBlocks.java
+++ src/main/java/au/com/sb/block/ModBlocks.java
@@ -1,4 +1,19 @@
 package au.com.sb.block;
 
+import au.com.sb.SbMod;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SbMod.MOD_ID);
+
+
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
 }
