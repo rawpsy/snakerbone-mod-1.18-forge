ddbd0beb0d291aca92f18e089833d5503edc7c2d
rawpsy <raxxtq@gmail.com>
2022-03-15 17:45:42 +1100

    Adds Cobalt Variant Blocks

---
+++ src/main/java/au/com/sb/item/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package au.com.sb.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+
+    public static final CreativeModeTab SB_TAB = new CreativeModeTab("sbtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.COBALT_INGOT.get());
+        }
+    };
+}
