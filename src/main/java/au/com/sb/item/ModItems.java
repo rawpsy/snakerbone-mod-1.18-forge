1c8fe8a6e2da1de3df39ece089c16508e891192e
rawpsy <raxxtq@gmail.com>
2022-03-15 17:46:11 +1100

    Update Cobalt Variants

--- src/main/java/au/com/sb/item/ModItems.java
+++ src/main/java/au/com/sb/item/ModItems.java
@@ -1,7 +1,6 @@
 package au.com.sb.item;
 
 import au.com.sb.SbMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,10 +12,12 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SbMod.MOD_ID);
 
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SB_TAB)));
 
     public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SB_TAB)));
+
+    // registry start
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
